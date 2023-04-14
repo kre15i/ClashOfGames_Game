72b2f0509e36d9cffcfc9fd557e5bbca3acf1e55
Erik <mail@erik-wendel.de>
2023-04-14 19:43:28 +0200

    I added jumping for the player // Dev_version(0.5)

--- scripts/playerController.gd
+++ scripts/playerController.gd
@@ -2,13 +2,16 @@ extends KinematicBody
 
 var direction = Vector3.FORWARD
 var movement_speed = 0
-var walk_speed = 5
-var run_speed = 10
+var walk_speed = 8
+var run_speed = 15
 var velocity = Vector3.ZERO
 var acceleration = 6
 var vertical_velocity = 0
-var gravity = 20
+var gravity = 25
 var angular_acceleration = 7
+var jump_magnitude = 15
+var weight_on_ground = 7
+
 
 func _input(event):
 	if event is InputEventKey:
@@ -32,11 +35,16 @@ func _physics_process(delta):
 		movement_speed = 0
 		
 	velocity = lerp(velocity, direction * movement_speed, delta * acceleration)
-	move_and_slide(velocity + Vector3.DOWN * vertical_velocity, Vector3.UP)
+	move_and_slide(velocity + Vector3.UP * vertical_velocity -get_floor_normal() * weight_on_ground, Vector3.UP)
 	
 	if !is_on_floor():
-		vertical_velocity += gravity * delta
+		vertical_velocity -= gravity * delta
 	else:
 		vertical_velocity = 0
 	
 	$Player.rotation.y = lerp_angle($Player.rotation.y, atan2(-direction.x, -direction.z), delta * angular_acceleration)
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("Jump"):
+			vertical_velocity = jump_magnitude
+	
